9727fad81d0e33a59ba3873788cc79cb831fd945
LWCoding <DiamondMob01@gmail.com>
2023-11-08 20:19:16 -0800

    Added steering wheel code (credited in script)
    Yet to be tested! :D

--- Assets/Scripts/SofaMoveController.cs
+++ Assets/Scripts/SofaMoveController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.XR;
 
@@ -7,8 +9,11 @@ using UnityEngine.XR;
 public class SofaMoveController : MonoBehaviour
 {
 
-    [Header("Object Assignment")]
-    public float SofaMoveSpeed;
+    [Header("Object Assignments")]
+    [SerializeField] private Transform _steeringWheelTransform;
+    [Header("Sofa Properties")]
+    [SerializeField] private float _sofaMoveSpeed;
+    [SerializeField] private float _sofaTurnSpeed;
 
     private InputDevice _rightController;
     private Rigidbody _rb;
@@ -16,6 +21,7 @@ public class SofaMoveController : MonoBehaviour
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        Debug.Assert(_steeringWheelTransform != null, "Steering wheel object has not been assigned!", this);
     }
 
     private void Start()
@@ -23,25 +29,34 @@ public class SofaMoveController : MonoBehaviour
         // Initialize the local _rightController variable.
         List<InputDevice> devices = new();
         InputDevices.GetDevicesAtXRNode(XRNode.RightHand, devices);
+        Debug.Assert(devices.Count > 0, "Player is not playing in VR!", this);  // Will be NULL if we're not playing in VR.
         _rightController = devices[0];
     }
 
     private void Update()
     {
-        HandleAcceleration();
-
-        // TODO: Remove this at the bottom. This is just a test!
-        if (Input.GetKey(KeyCode.W))
+#if UNITY_EDITOR
+        // If we're in the Unity Editor, take the W/A/D keys.
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            _rb.AddForce(Vector3.forward * SofaMoveSpeed);
+            _rb.AddForce(Vector3.forward * _sofaMoveSpeed, ForceMode.Impulse);
         }
+        float hor = Input.GetAxisRaw("Horizontal");
+        if (hor != 0)
+        {
+            transform.Rotate(0, hor * _sofaTurnSpeed * Time.deltaTime, 0);
+        }
+#else
+        // If we're in VR, use the functions created below.
+        HandleAcceleration();
+        HandleTurning();
+#endif
     }
 
-    /* Function: HandleAcceleration
-     * ------------------------------
-     * This function checks if the Trigger button is being pressed on the
-     * controller and adds a forward acceleration to the object.
-     */
+    /// <summary>
+    /// Checks if the Trigger button is being pressed on the controller and adds 
+    /// a forward acceleration to the object.
+    /// </summary>
     private void HandleAcceleration()
     {
         bool triggerValue;  // Temp variable to hold trigger button status!
@@ -50,5 +65,13 @@ public class SofaMoveController : MonoBehaviour
             // Code is run when the trigger button is pressed.
             Debug.Log("I have been triggered!!!");
         }
+    }
+
+    /// <summary>
+    /// Checks if the steering wheel is rotated and makes the car rotate accordingly.
+    /// </summary>
+    private void HandleTurning()
+    {
+        
     }
 }
